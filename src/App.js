731d35a0426c650a45fd20a7764868fcff3fe10b
amal-san <amaldemaniax@gmail.com>
2022-06-30 06:47:44 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  
+  const [num1 , setNum1 ] = useState(null);
+  const [num2 , setNum2 ] = useState(null);
+
+  const setNumbers = () => {
+    setNum1(getNum())
+    setNum2(getNum())
+  }
+
+  useEffect(() => {
+    const subscribe = setNumbers();
+  },[])
+
+  const getNum = ( ) => {
+    return Math.floor(Math.random()*(99-10+1)+10);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header></header>
+      <section>
+        <h1 style={{textAlign:'center'}}>Addition</h1>
+        <section>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <p><span>{num1}</span> + <span>{num2}</span></p>
+              </div>
+              <div className="flip-card-back">
+               <p>{num1 + num2}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section style={{textAlign:'center'}}> 
+         <button className='btn' onClick={() => setNumbers()}>Next</button>
+        </section>
+      </section>
     </div>
   );
 }
